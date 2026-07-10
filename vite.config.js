0038989ff6ff0772572d05e66ce44699d9ff1d1d
Mingming-up <279415660+Mingming-up@users.noreply.github.com>
2026-07-10 21:17:53 +0800

    Fix Sites static output path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
+    outDir: "dist/client",
     rollupOptions: {
       input: {
         home: resolve(import.meta.dirname, "index.html"),
